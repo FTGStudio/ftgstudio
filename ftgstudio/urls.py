33cb4709542d98520cb9f0d1bba67d0dfb6fe8ea
FTGStudio <doranm@mail.gvsu.edu>
2016-11-20 15:56:25 -0500

    added templates and simple model scheme to prove out blog creation

--- ftgstudio/urls.py
+++ ftgstudio/urls.py
@@ -17,5 +17,15 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'blog.views.index'),
     url(r'^admin/', admin.site.urls),
+    url(
+        r'^blog/view/(?P<slug>[^\.]+).html',
+        'blog.views.view_post',
+        name='view_blog_post'),
+    url(
+        r'^blog/category/(?P<slug>[^\.]+).html',
+        'blog.views.view_category',
+        name='view_blog_category'),
+
 ]
